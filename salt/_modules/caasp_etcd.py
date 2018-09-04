4b77e16482be44c0a1395209f16d26671e8a28fe
Florian Bergmann <fbergmann@suse.de>
2018-09-04 17:21:09 +0200

    Do not ignore etcd members if they were requested without id.

--- salt/_modules/caasp_etcd.py
+++ salt/_modules/caasp_etcd.py
@@ -171,6 +171,8 @@ def get_endpoints(with_id=False, skip_this=False, skip_removed=False, port=ETCD_
         if with_id:
             member_endpoint = "{}={}".format(node_id, member_endpoint)
             etcd_members_lst.append(member_endpoint)
+        else:
+            etcd_members_lst.append(member_endpoint)
 
     if len(etcd_members_lst) == 0:
         __utils__['caasp_log.error']('no etcd members available!!')
